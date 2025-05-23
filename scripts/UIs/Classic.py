be39e12ac863e90e65786ea4550d5f593780ab1b
anxety-solo <YuukiSanNya@yandex.ru>
2025-05-23 15:53:50 +0900

    W

--- scripts/UIs/Classic.py
+++ scripts/UIs/Classic.py
@@ -69,7 +69,6 @@ async def download_configuration():
         f"{url_cfg}/styles.csv",
         f"{url_cfg}/user.css",
         # other
-        f"{url_cfg}/card-no-preview.png, {WEBUI}/html",
         f"{url_cfg}/notification.mp3"
     ]
     await download_files(configs)
