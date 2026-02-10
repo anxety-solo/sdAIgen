e5033c0c4499ef1d778824275164c19a3c3d1b99
anxety-solo <YuukiSanNya@yandex.ru>
2026-02-10 21:45:16 +0900

    aa

--- scripts/en/downloading-en.py
+++ scripts/en/downloading-en.py
@@ -152,9 +152,9 @@ if not SKIP_INSTALL_VENV and venv_needs_reinstall:
         'Neo':     (f"{HF_REPO_URL}/python31312-venv-torch2100-cu128-C-Neo.tar.lz4", 'Neo • 3.13.12'),
         'Classic': (f"{HF_REPO_URL}/python31113-venv-torch260-cu124-C-Classic.tar.lz4", 'Classic • 3.11.13'),
         'ComfyUI': (f"{HF_REPO_URL}/python31018-venv-torch260-cu124-C-ComfyUI.tar.lz4", 'ComfyUI • 3.10.18'),
-        'default': (f"{HF_REPO_URL}/python31018-venv-torch260-cu124-C-fa.tar.lz4", 'default • 3.10.18')
+        'default': (f"{HF_REPO_URL}/python31018-venv-torch260-cu124-C-fa.tar.lz4", 'Default • 3.10.18')
     }
-    venv_url, venv_version = venv_config.get(current_ui, venv_config['Default'])
+    venv_url, venv_version = venv_config.get(current_ui, venv_config['default'])
 
     print(f"♻️ Installing VENV: {COL.B}{venv_version}{COL.X}, this may take a while...")
     setup_venv(venv_url)
